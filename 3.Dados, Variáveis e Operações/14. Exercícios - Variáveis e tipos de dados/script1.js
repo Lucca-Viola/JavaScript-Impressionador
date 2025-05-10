d539ce6389c0f7c83ddf85bcc414bad917cf4f61
Lucca Viola <luccaviola05@gmail.com>
2025-05-09 21:58:38 -0400

    Exercício finalizado

---
+++ 3.Dados, Variáveis e Operações/14. Exercícios - Variáveis e tipos de dados/script1.js
@@ -0,0 +1,30 @@
+//Primeiro e segundo exercício
+let mensagem = "Olá, Mundo!";
+
+console.log(mensagem);
+
+//Terceiro exercício
+let nome = "Lucca";
+let idade = 20;
+let isStudante = true;
+
+console.log(nome, "tem", idade, "anos e é estudante?", isStudante);
+
+//Quinto exercício
+const PI = 3.14;
+console.log("O valor de PI é:", PI);
+
+//Sexto exercício
+let endereco = undefined;
+let telefone = undefined;
+
+//Setimo exercício
+let nomeProduto = "Tênis";
+let precoProduto = 199.99;
+let quantidadeProduto = 2;
+
+//Oitavo exercício
+let nomeCidade = "Matupá";
+let populacaoCidade = 20000;
+
+console.log(`A cidade chamada ${nomeCidade} tem ${populacaoCidade} habitantes`);
