5dfdf7debd1c66f9dba0317ff5984711ee7c9235
Lucca.Viola <luccaviola05@gmail.com>
2025-05-14 11:27:43 -0300

    Operadores aritméticos parte 2

---
+++ 3.Dados, Variáveis e Operações/17. Operadores Aritméticos - Parte 2/script.js
@@ -0,0 +1,13 @@
+// Adição + | Subtração - | Multiplicação * | Divisão / | Módulo % | Inversão de sinal/negativo -
+
+//Operadores Aritméticos avançados
+let base = 2;
+let expoente = 3;
+const resultado = base ** expoente; // 2 sendo elevado a 3
+console.log(resultado);
+
+// Incremento ++ (adicionar o valor 1) | Decremento -- (diminuir o valor 1)
+let controleUm = 0;
+let controleDois = 0;
+console.log(++controleUm);
+console.log(--controleDois);
