c62d6956d0a1b27e8c77b92bc9b539ce5919398b
Lucca.Viola <luccaviola05@gmail.com>
2025-05-14 12:06:59 -0300

    Operadores de comparação

---
+++ 3.Dados, Variáveis e Operações/18. Operadores de comparanção/script.js
@@ -0,0 +1,17 @@
+//Igualdade e Estrita Igualdade | Diferença e Estrita Diferença
+console.log(5 == "5");
+console.log(5 === "5"); // boa prática
+
+console.log(5 != "5");
+console.log(5 !== "5"); // boa prática
+
+// Maior e Menor
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 >= 10);
+
+// Exemplo
+let senha = 1234;
+let senhaDigitada = 12345;
+let comparacao = senha === senhaDigitada;
+console.log("A minha senha é: " + comparacao);
