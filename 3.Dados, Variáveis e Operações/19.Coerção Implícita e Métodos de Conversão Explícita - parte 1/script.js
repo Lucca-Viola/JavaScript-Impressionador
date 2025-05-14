11cd07027fcecad64a964b8c4ea648ccef6975a8
Lucca.Viola <luccaviola05@gmail.com>
2025-05-14 12:26:55 -0300

    Coerção Implicita parte 1

---
+++ 3.Dados, Variáveis e Operações/19.Coerção Implícita e Métodos de Conversão Explícita - parte 1/script.js
@@ -0,0 +1,6 @@
+// Coerção Implícita
+console.log(5 == "5"); // igualdade - coersão implícita
+console.log(5 === "5");
+console.log(5 + "5"); // number é convertido para string
+console.log("5" - 5); // string é convertida para number
+console.log("3" * "2"); // strings são convertidas para number
