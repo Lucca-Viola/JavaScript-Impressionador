60127fd18e087beaeb79e453fe19c4208b1871ec
Lucca.Viola <luccaviola05@gmail.com>
2025-05-14 11:27:21 -0300

    Operadores aritméticos parte 1

---
+++ 3.Dados, Variáveis e Operações/16. Operadores Aritméticos - Parte 1/script.js
@@ -0,0 +1,26 @@
+// Operador |     Operação    | Exemplo
+//    +     |      Adição     |  X + Y
+//    -     |    Subtração    |  X - Y
+//    *     |  Multiplicação  |  X * Y
+//    /     |     Divisão     |  X / Y
+//    %     |    Resto/Div    |  X % Y
+//    -     |   Invers.sinal  |   -X
+//   ++     |    Incremento   |X++ ou ++XX
+//   --     |    Decremento   |X-- ou --X
+
+//Adição
+let numeroUm = 10;
+let numeroDois = 43;
+console.log(numeroUm + numeroDois);
+
+//let mensagem = "20";
+//console.log(mensagem + numeroUm); // Contatenação
+
+// Subtração
+console.log(numeroDois - numeroUm);
+
+// Divisão
+console.log(numeroUm / numeroDois);
+
+// Módulo (Resto da divisão)
+console.log(numeroDois % numeroUm);
