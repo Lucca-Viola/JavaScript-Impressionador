47eff50a05a9b8e2cd8dd304224cce2ffb8eea2d
Lucca.Viola <luccaviola05@gmail.com>
2025-05-22 10:12:39 -0400

    Chamando Funções

---
+++ 4. Funções/2. Chamando Funções/script.js
@@ -0,0 +1,15 @@
+// Chamadas de funções
+
+function exibirDetalhes(nome, curso) {
+  console.log("Professor: " + nome + " Curso: " + curso); // instrução
+}
+
+// exibirDetalhes("Ricardo", "Sistema da Informação");
+// exibirDetalhes("Lira", "Python");
+// exibirDetalhes("Daniel", "JavaScript");
+
+function mensagem() {
+  console.log("Mensagem");
+}
+
+mensagem();
