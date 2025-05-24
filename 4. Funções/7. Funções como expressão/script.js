ac11c9c351ce851a008441c68adf34b8f17e638f
Lucca Viola <luccaviola05@gmail.com>
2025-05-24 08:55:06 -0400

    Funções como expressão

---
+++ 4. Funções/7. Funções como expressão/script.js
@@ -0,0 +1,9 @@
+// Declaração e Expressão de Função
+
+let total = function soma(a, b) {
+  return a + b;
+};
+
+console.log(total(5, 10)); // 15
+// A função soma é uma expressão de função, pois está atribuída a uma variável.
+// Isso significa que ela não pode ser chamada antes de ser definida.
