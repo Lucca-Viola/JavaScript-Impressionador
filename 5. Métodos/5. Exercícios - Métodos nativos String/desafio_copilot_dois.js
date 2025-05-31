343423bac325727af989ad7956c9d974fdc37dc0
Lucca Viola <luccaviola05@gmail.com>
2025-05-31 13:41:52 -0400

    Mais desafíos

---
+++ 5. Métodos/5. Exercícios - Métodos nativos String/desafio_copilot_dois.js
@@ -0,0 +1,43 @@
+// Desafio Um
+let desafioUm = "Javascript é divertido";
+console.log(desafioUm.toLowerCase());
+
+// Desafio Dois
+let desafioDois = "Aprenda a programar";
+console.log(desafioDois.substring(10, 19));
+
+// Desafio Três
+let desafioTres = "banana, maça, laranja";
+console.log(desafioTres.replace("maça", "uva"));
+
+// Desafio Quatro
+let desafioQuatro = "sol lua estrela";
+console.log(desafioQuatro.split(" "));
+
+// Desafio Cinco
+let desafioCinco = "12345";
+console.log(desafioCinco.slice(0, 3));
+
+// Desafio Seis
+let desafioSeis = "cachorro, gato, peixe, pássaro";
+desafioSeis = desafioSeis.split(", ");
+desafioSeis.splice(2, 1);
+console.log(desafioSeis);
+
+// Desafio Sete
+let desafioSete = "Meu nome é Ana";
+console.log(desafioSete.indexOf("Ana"));
+
+// Desafio Oito
+let desafioOito = "verde, azul, amarelo, vermelho";
+desafioOito = desafioOito.split(", ");
+desafioOito.splice(1, 1);
+console.log(desafioOito);
+
+// Desafio Nove
+let desafioNove = "Python, JavaScript, Java";
+console.log(desafioNove.replace(",", ";"));
+
+// Desafio Dez
+let desafioDez = "abcdefg";
+console.log(desafioDez.substring(2, 5));
